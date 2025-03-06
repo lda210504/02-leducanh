789b3293c331d455b2d153be71d04bf89d4845b4
lda210504 <leducanh.10042006@gmail.com>
2025-03-06 22:49:04 +0700

    bai1

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    printf("x^2: %d\n", x * x);
+    printf("x^3: %d\n", x * x * x);
+    printf("x^4: %d\n", x * x * x * x);
+    return 0;
+}
